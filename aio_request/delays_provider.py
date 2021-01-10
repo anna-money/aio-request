ec9ca09932378ca08496dfcb559c6aac0a919884
Yury Pliner <yury.pliner@gmail.com>
2021-01-10 11:42:31 +0500

    Renames

--- aio_request/delays_provider.py
+++ aio_request/delays_provider.py
@@ -15,10 +15,10 @@ def constant_delays(*, delay: float = 0, jitter: float = 0.2) -> DELAY_PROVIDER:
 
 
 def linear_delays(
-    *, min_delay_seconds: float = 0, delay_between_attempts: float = 0.05, jitter: float = 0.2
+    *, min_delay_seconds: float = 0, delay_multiplier: float = 0.05, jitter: float = 0.2
 ) -> DELAY_PROVIDER:
     def _linear(attempt: int) -> float:
-        delay = min_delay_seconds + attempt * delay_between_attempts
+        delay = min_delay_seconds + attempt * delay_multiplier
         jitter_amount = delay * random() * jitter
         if random() < 0.5:
             jitter_amount = -jitter_amount
--- tests/test_delays.py
+++ tests/test_delays.py
@@ -9,7 +9,7 @@ def test_constant():
 
 
 def test_linear():
-    delays_provider = linear_delays(min_delay_seconds=1, delay_between_attempts=2, jitter=0)
+    delays_provider = linear_delays(min_delay_seconds=1, delay_multiplier=2, jitter=0)
     assert delays_provider(0) == 1
     assert delays_provider(1) == 3
     assert delays_provider(2) == 5
