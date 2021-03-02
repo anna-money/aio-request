4aa43ac237839749e5f7528df7c380593663c0ae
Yury Pliner <yury.pliner@gmail.com>
2021-03-02 14:00:36 +0500

    Fix tests

--- aio_request/__init__.py
+++ aio_request/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-__version__ = "0.0.23"
+__version__ = "0.0.24"
 
 from .base import ClosableResponse, EmptyResponse, Header, Method, Request, Response
 from .client import Client, setup
