671a174e29416a67eeb4cbb11c2217b135a0096a
Yury Pliner <yury.pliner@gmail.com>
2021-04-06 13:43:19 +0400

    Fix asyncio.Future typing hell

--- aio_request/utils.py
+++ aio_request/utils.py
@@ -1,5 +1,6 @@
 import asyncio
 import contextlib
+import sys
 from typing import Any, Callable, Collection, Dict, Iterable, List, Mapping, Optional, Protocol, TypeVar, Union
 
 import yarl
@@ -23,27 +24,50 @@ async def close_single(item: TClosable) -> None:
 T = TypeVar("T")
 
 
-async def close_futures(items: Collection["asyncio.Future[T]"], as_close: Callable[[T], TClosable]) -> None:
-    for item in items:
-        if item.cancelled():
-            continue
-        try:
-            await close_single(as_close(await item))
-        except asyncio.CancelledError:
-            if not item.cancelled():
-                raise
+if sys.version_info >= (3, 9, 0):
+
+    async def close_futures(items: Collection[asyncio.Future[T]], as_close: Callable[[T], TClosable]) -> None:
+        for item in items:
+            if item.cancelled():
+                continue
+            try:
+                await close_single(as_close(await item))
+            except asyncio.CancelledError:
+                if not item.cancelled():
+                    raise
+
+    async def cancel_futures(futures: Collection[asyncio.Future[T]]) -> None:
+        for future in futures:
+            if future.done():
+                continue
+            future.cancel()
+
+
+else:
+
+    async def _close_futures_py38(items: Collection[Any], as_close: Callable[[Any], TClosable]) -> None:
+        for item in items:
+            if item.cancelled():
+                continue
+            try:
+                await close_single(as_close(await item))
+            except asyncio.CancelledError:
+                if not item.cancelled():
+                    raise
+
+    async def _cancel_futures_py38(futures: Collection[Any]) -> None:
+        for future in futures:
+            if future.done():
+                continue
+            future.cancel()
+
+    close_futures = _close_futures_py38
+    cancel_futures = _cancel_futures_py38
 
 
 async def close(items: Collection[TClosable]) -> None:
     for item in items:
         await close_single(item)
-
-
-async def cancel_futures(futures: Collection[asyncio.Future[T]]) -> None:
-    for future in futures:
-        if future.done():
-            continue
-        future.cancel()
 
 
 def substitute_path_parameters(url: yarl.URL, parameters: Optional[PathParameters] = None) -> yarl.URL:
