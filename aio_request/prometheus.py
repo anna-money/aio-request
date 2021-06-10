63eb4f46a3917bbc0a8b37cd7d955d5d5565f51f
Yury Pliner <yury.pliner@gmail.com>
2021-06-10 12:01:03 +0500

    Add observation buckets

--- aio_request/prometheus.py
+++ aio_request/prometheus.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Collection
 
 import prometheus_client
 
@@ -8,9 +8,15 @@ from .metrics import MetricsProvider
 class PrometheusMetricsProvider(MetricsProvider):
     __slots__ = ("_metrics", "_registry")
 
-    def __init__(self, registry: prometheus_client.CollectorRegistry) -> None:
+    def __init__(
+        self,
+        registry: prometheus_client.CollectorRegistry,
+        *,
+        observation_buckets: Collection[float] = prometheus_client.Histogram.DEFAULT_BUCKETS
+    ) -> None:
         self._metrics: Dict[str, Any] = {}
         self._registry = registry
+        self._observation_buckets = observation_buckets
 
     def increment_counter(self, name: str, tags: Dict[str, Any], value: float = 1) -> None:
         if name not in self._metrics:
@@ -19,7 +25,12 @@ class PrometheusMetricsProvider(MetricsProvider):
 
     def observe_value(self, name: str, tags: Dict[str, Any], value: float) -> None:
         if name not in self._metrics:
-            self._metrics[name] = prometheus_client.Histogram(name, "", labelnames=tags.keys())
+            self._metrics[name] = prometheus_client.Histogram(
+                name,
+                "",
+                labelnames=tags.keys(),
+                buckets=self._observation_buckets,
+            )
         self._metrics[name].labels(*[str(value) for value in tags.values()]).observe(value)
 
 
