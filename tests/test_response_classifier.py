49f63ba12fd4da8573c8ac5496bc7200f73b1102
Yury Pliner <yury.pliner@gmail.com>
2022-01-06 16:02:12 +0000

    Allow to customize DefaultResponseClassifier

---
+++ tests/test_response_classifier.py
@@ -0,0 +1,19 @@
+import pytest
+
+import aio_request
+
+
+@pytest.mark.parametrize(
+    "status, verdict_for_status, verdict",
+    [
+        (408, None, aio_request.ResponseVerdict.REJECT),
+        (400, None, aio_request.ResponseVerdict.ACCEPT),
+        (429, None, aio_request.ResponseVerdict.ACCEPT),
+        (429, {429: aio_request.ResponseVerdict.REJECT}, aio_request.ResponseVerdict.REJECT),
+    ],
+)
+def test_default_response_classifier(
+    status: int, verdict_for_status: dict[int, aio_request.ResponseVerdict], verdict: aio_request.ResponseVerdict
+) -> None:
+    classifier = aio_request.DefaultResponseClassifier(verdict_for_status=verdict_for_status)
+    assert classifier.classify(aio_request.EmptyResponse(status=status)) == verdict
