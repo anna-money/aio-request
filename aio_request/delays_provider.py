a082ab86274f5b70c5e746c53d2394ae475b3742
Iurii Pliner <yury.pliner@gmail.com>
2024-08-27 21:23:53 +0100

    Renames

--- aio_request/delays_provider.py
+++ aio_request/delays_provider.py
@@ -1,24 +1,24 @@
 import collections.abc
 import random
 
-DELAYS_PROVIDER = collections.abc.Callable[[int], float]
+DelaysProvider = collections.abc.Callable[[int], float]
 
 
-def constant_delays(*, delay: float = 0) -> DELAYS_PROVIDER:
+def constant_delays(*, delay: float = 0) -> DelaysProvider:
     return lambda _: delay
 
 
 def linear_backoff_delays(
     *, min_delay_seconds: float = 0, delay_multiplier: float = 0.05, jitter: float = 0.2
-) -> DELAYS_PROVIDER:
-    def _linear(attempt: int) -> float:
+) -> DelaysProvider:
+    def __linear_backoff_delays(attempt: int) -> float:
         delay = min_delay_seconds + attempt * delay_multiplier
         jitter_amount = delay * random.random() * jitter
         if random.random() < 0.5:
             jitter_amount = -jitter_amount
         return delay + jitter_amount
 
-    return _linear
+    return __linear_backoff_delays
 
 
 linear_delays = linear_backoff_delays
