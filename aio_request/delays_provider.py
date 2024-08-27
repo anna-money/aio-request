4207a97c9034e68547b1c172c83911c54d126cad
Iurii Pliner <yury.pliner@gmail.com>
2024-08-27 20:41:00 +0100

    Use DELAYS_PROVIDER where possible

--- aio_request/delays_provider.py
+++ aio_request/delays_provider.py
@@ -1,16 +1,16 @@
 import collections.abc
 import random
 
-DELAY_PROVIDER = collections.abc.Callable[[int], float]
+DELAYS_PROVIDER = collections.abc.Callable[[int], float]
 
 
-def constant_delays(*, delay: float = 0) -> DELAY_PROVIDER:
+def constant_delays(*, delay: float = 0) -> DELAYS_PROVIDER:
     return lambda _: delay
 
 
 def linear_backoff_delays(
     *, min_delay_seconds: float = 0, delay_multiplier: float = 0.05, jitter: float = 0.2
-) -> DELAY_PROVIDER:
+) -> DELAYS_PROVIDER:
     def _linear(attempt: int) -> float:
         delay = min_delay_seconds + attempt * delay_multiplier
         jitter_amount = delay * random.random() * jitter
